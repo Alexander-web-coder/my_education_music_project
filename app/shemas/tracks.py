c60f0b9f3a2ec3ce5fb44bced621e744be553e48
Alexander-web-coder <shurikl@list.ru>
2025-04-23 19:29:02 +0400

    Добавлено описание классов для базовых операций.

--- app/shemas/tracks.py
+++ app/shemas/tracks.py
@@ -1,7 +1,18 @@
 from pydantic import BaseModel
 
 class Track(BaseModel): #TODO
-    pass
+    title: str
+    author: str
+    genre: list[str] = None
 
-class User(BaseModel) #TODO
-    pass
+
+class User(BaseModel): #TODO
+    login: str
+    first_name: str
+    last_name: str
+    #hash: str надо подумать
+
+
+class ratings(Track, User): #TODO
+    """отправляет название и автора трека, оценку и логин"""
+    estimate: int

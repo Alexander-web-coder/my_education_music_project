35d053dc3b4087bcd786de7fd709bd30463f257d
Alexander-web-coder <shurikl@list.ru>
2025-04-23 18:04:48 +0400

    Добавлены статус-коды в роутер.

--- app/routes/track.py
+++ app/routes/track.py
@@ -1,27 +1,34 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
+
 
 router = APIRouter(prefix="/tracks", tags=["Операции с треками"])
 
-@router.get("/get_full_list")
-def get_full_list(): #TODO
+
+@router.get("/get_full_list", status_code=status.HTTP_200_OK)
+def get_full_list():  #TODO
     pass
 
-@router.get("/get_top")
-def get_top(): #TODO
+
+@router.get("/get_top", status_code=status.HTTP_200_OK)
+def get_top():  #TODO
     pass
 
-@router.get("/get_my_recommend")
-def get_my_recommend(): #TODO
+
+@router.get("/get_my_recommend", status_code=status.HTTP_200_OK)
+def get_my_recommend():  #TODO
     pass
 
-@router.post("/create_track")
-def create_track(): #TODO
+
+@router.post("/create_track", status_code=status.HTTP_201_CREATED)
+def create_track():  #TODO
     pass
 
-@router.patch("/set_rating")
-def set_rating(): #TODO
+
+@router.patch("/set_rating", status_code=status.HTTP_202_CREATED)
+def set_rating():  #TODO
     pass
 
-@router.delete("/delete_track")
-def delete_track(): #TODO
+
+@router.delete("/delete_track", status_code=status.HTTP_204_NO_CONTENT)
+def delete_track():  #TODO
     pass
--- app/routes/users.py
+++ app/routes/users.py
@@ -1,11 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 
 router = APIRouter(prefix="/users", tags=["Операции с пользователями"])
 
-@router.post("/create_user")
+@router.post("/create_user", status_code=status.HTTP_201_CREATED)
 def create_user(): #TODO
     pass
 
-@router.delete("/delete_user")
+@router.delete("/delete_user", status_code=status.HTTP_204_NO_CONTENT)
 def delete_user(): #TODO
     pass

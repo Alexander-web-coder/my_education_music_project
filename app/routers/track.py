ffe13710388c68e75e00b7260679fb0f0d109e8f
Alexander-web-coder <shurikl@list.ru>
2025-04-28 22:23:54 +0400

    Включена проверка на уникальность при записи в таблицу оценок.

--- app/routers/track.py
+++ app/routers/track.py
@@ -1,11 +1,24 @@
-from fastapi import APIRouter, status, Depends
+#from http.client import HTTPException
+
+from fastapi import APIRouter, status, Depends, HTTPException, Request
 from sqlmodel import Session, select, text
+from fastapi.responses import JSONResponse
+from sqlalchemy.exc import SQLAlchemyError, InternalError, IntegrityError
 from app.db import  get_session
 from app.models.models import Track as Track_db
 from app.schemas.tracks import Track, DeleteTrack
+#from app.main import app
 
 
 router = APIRouter(prefix="/tracks", tags=["Операции с треками"])
+
+# @app.exception_handler(SQLAlchemyError)
+# async def sqlalchemy_exception_handler(request: Request, exc: SQLAlchemyError):
+#     # Для ошибок целостности (например, дублирование уникального ключа)
+#     return JSONResponse(
+#             status_code=400,
+#             content={"message": "Возможно, запись уже существует или нарушены ограничения базы данных"},
+#         )
 
 # для теста базы
 @router.get("/test-db", status_code=status.HTTP_200_OK)
@@ -27,17 +40,23 @@ def create_track(track: Track, session: Session = Depends(get_session)):
         author = track.author,
         genre = track.genre
     )
-    session.add(new_track)
-    session.commit()
-    session.refresh(new_track)
+    try:
+        session.add(new_track)
+        session.commit()
+        session.refresh(new_track)
+    except SQLAlchemyError as e:
+        # raise HTTPException(status_code=status.HTTP_100_CONTINUE,
+        #                     detail="Ошибка записи в базу. Возможно, оценка уже поставлена.")
+        raise
+
     return track
 
 
 @router.delete("/delete_track", status_code=status.HTTP_204_NO_CONTENT)
 def delete_track(track_id: DeleteTrack, session: Session = Depends(get_session)):
     # for_delete = session.exec(select(Track_db).where(Track_db.id == track_id.id))
-    statement = select(Track_db).where(Track_db.id == track_id.id)
-    results = session.exec(statement)
+    stmt = select(Track_db).where(Track_db.id == track_id.id)
+    results = session.exec(stmt)
     for_delete = results.one()
     session.delete(for_delete)
     session.commit()

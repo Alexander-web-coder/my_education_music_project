ea81e847ab03ff541f20ea3ba440b9a9fcba9285
Alexander-web-coder <shurikl@list.ru>
2025-05-01 15:25:29 +0400

    Причесывание pylint №9

--- app/routers/ratings.py
+++ app/routers/ratings.py
@@ -13,7 +13,8 @@ func: Callable
 router = APIRouter(prefix="/ratings", tags=["Операции с оценками"])
 
 @router.patch("/set_rating", status_code=status.HTTP_201_CREATED) #TODO
-def set_rating(rating: Ratings, login=Depends(get_current_user), session=Depends(get_session)) -> Rating_db:
+def set_rating(rating: Ratings, login=Depends(get_current_user),
+               session=Depends(get_session)) -> Rating_db:
     """Устанавливает оценку трека, требуется логин юзера"""
     # statement = select(User).where(User.login == login.login)
     # user_exist_id = session.exec(statement).first()
@@ -37,7 +38,8 @@ def set_rating(rating: Ratings, login=Depends(get_current_user), session=Depends
 
 
 @router.patch("/change_rating", status_code=status.HTTP_201_CREATED)
-def change_rating(rating: Ratings, login=Depends(get_current_user), session=Depends(get_session)): # -> Rating_db:
+def change_rating(rating: Ratings, login=Depends(get_current_user),
+                  session=Depends(get_session)): # -> Rating_db:
     """Меняет оценку трека, требуется логин юзера"""
     # statement = select(User).where(User.login == login.login)
     # user_exist_id = session.exec(statement).first()
@@ -137,63 +139,3 @@ def get_my_recommend(user_id: int, session: Session = Depends(get_session)) -> L
     )
 
     return session.exec(stmt).all()
-
-# def get_my_recommend(login=Depends(get_current_user), session=Depends(get_session)):
-# def get_music_recommendations(user_id: int, session: Session = Depends(get_session)) -> List[Track]:
-# def get_music_recommendations(user_id: int, session: Session = Depends(get_session)):
-#     """Возвращает список рекомендованных треков """
-#
-#     # 1. Определяем 2 самых любимых жанра
-#     user_genres_subq = (
-#         select(Track.genre)
-#         .join(Rating_db, Rating_db.track_id == Track.id)
-#         .where(and_(
-#             Rating_db.user_id == user_id,
-#             Rating_db.estimate >= 4
-#         ))
-#         .group_by(Track.genre)
-#         .order_by(func.count().desc())
-#         .limit(2)
-#         .subquery()
-#     )
-#
-#     # 2. Находим ID
-#     similar_users_subq = (
-#         select(Rating_db.user_id)
-#         .join(Track, Track.id == Rating_db.track_id)
-#         .where(and_(
-#             Rating_db.user_id != user_id,
-#             Rating_db.estimate >= 4,
-#             Track.genre.in_(select(user_genres_subq.c.genre))
-#         ))
-#         .group_by(Rating_db.user_id)
-#         .having(func.count() >= 2)
-#         .subquery()
-#     )
-#
-#     # 3. Получаем рекомендации треков
-#     recommendations = session.exec(
-#         select(Track)
-#         .join(Rating_db, Rating_db.track_id == Track.id)
-#         .where(and_(
-#             Rating_db.user_id.in_(select(similar_users_subq.c.user_id)),
-#             Rating_db.estimate >= 4,
-#             Track.genre.in_(select(user_genres_subq.c.genre)),
-#             not_(exists(
-#                 select(Rating_db.id)
-#                 .where(and_(
-#                     Rating_db.user_id == user_id,
-#                     Rating_db.track_id == Track.id
-#                 ))
-#             ))
-#         ))
-#         .group_by(Track.id)
-#         .order_by(
-#             func.count().desc(),
-#             func.avg(Rating_db.estimate).desc()
-#         )
-#         .limit(3)
-#     ).all()
-#
-#     # return recommendations
-#     return None

fd3f760522de239c5e20ef396f0460da20680b4a
Alexander-web-coder <shurikl@list.ru>
2025-05-03 20:21:52 +0400

    Добавлена приветственная страница.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 """Главный модуль"""
 from contextlib import asynccontextmanager
-from fastapi import FastAPI, status
+from fastapi import FastAPI, Request, status
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from app.routers import tracks, users, ratings
 from .db import init_database
 
@@ -28,12 +30,17 @@ app = FastAPI(
     }
 )
 
+templates = Jinja2Templates(directory="app/templates")
 
+@app.get("/", status_code=status.HTTP_200_OK, response_class=HTMLResponse)
+async def root(request: Request):
+    """Вывод страницы с приветствием"""
+    return templates.TemplateResponse(request, "index.html")
 
-@app.get("/", status_code=status.HTTP_200_OK, tags=["Главная страница"])
-def root() -> dict:
-    """Заглавная страница"""
-    return {"Для информации":"перейдите на http://127.0.0.1:80/docs"}
+# @app.get("/", status_code=status.HTTP_200_OK, tags=["Главная страница"])
+# def root() -> dict:
+#     """Заглавная страница"""
+#     return {"Для информации":"перейдите на http://127.0.0.1:80/docs"}
 
 
 app.include_router(tracks.router)
